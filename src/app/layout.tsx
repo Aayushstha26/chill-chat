1f864e71c167d5a25f3c3633473e8d442a4be897
Aayush shrestha <sthaaayush375@gmail.com>
2026-05-15 13:14:30 +0545

    feat: integrate next-auth for user authentication and add AuthPage UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { AuthProvider } from "@/context/authContextProvider";
+import AuthProvider from '@/components/Providers/authProvider'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
